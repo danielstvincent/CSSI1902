b241e4a9ae3ca614eb061cf9dc9eb74f01f8c580
danielstvincent <danielstvincent@mec.science>
2019-07-30 10:08:45 -0400

    day6

---
+++ CSSI1902/day6/task1/script.js
@@ -0,0 +1,10 @@
+const CountToN=(x)=>
+{
+    var absX=Math.abs(x);
+    let index=1;
+    while(index<=absX)
+    {
+        console.log(index);
+        index=index+1; // index++;index+=1
+    }
+}
